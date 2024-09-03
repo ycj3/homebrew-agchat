e784cccaab1df33cafc37dfe2c7f26da5b40fa95
Carlson <carlsonyuandev@gmail.com>
2024-09-03 13:55:51 +0800

    add agchat Formula

---
+++ Formula/agchat.rb
@@ -0,0 +1,22 @@
+class Agchat < Formula
+    desc "A command-line interface for Agora Chat"
+    homepage "https://github.com/ycj3/agora-chat-cli"
+    url "https://github.com/ycj3/agora-chat-cli/archive/refs/tags/v0.1.0.tar.gz"
+    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    license "MIT"
+  
+    depends_on "go" => :build
+  
+    def install
+        if Hardware::CPU.arm?
+          system "go", "build", *std_go_args(ldflags: "-s -w -buildmode=pie")
+        elsif Hardware::CPU.intel?
+          system "go", "build", *std_go_args(ldflags: "-s -w")
+        end
+      end
+  
+    test do
+      assert_match "agchat", shell_output("#{bin}/agchat --version")
+    end
+  end
+  
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # homebrew-agchat
+
+A Homebrew repository that hosts cool software. You can add this repository with `brew tap ycj3/agchat`.
+
+## Formulae
+`agchat` is a command-line interface (CLI) tool for managing Agora chat applications.
+```
+brew install agchat
+```
