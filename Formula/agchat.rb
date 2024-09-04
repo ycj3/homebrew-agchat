70a2896bd1801c332087c70a2e0fb281bcb1b56d
Carlson <carlsonyuandev@gmail.com>
2024-09-04 08:36:09 +0000

    update agchat to v0.2.1

--- Formula/agchat.rb
+++ Formula/agchat.rb
@@ -1,8 +1,8 @@
 class Agchat < Formula
     desc "A command-line interface for Agora Chat"
     homepage "https://github.com/ycj3/agora-chat-cli"
-    url "https://github.com/ycj3/agora-chat-cli/archive/v0.2.0.tar.gz"
-    sha256 "c29e7c512a48d86c10aa0be9266cd6bf092bad9716e3dc3a48691d51f5a03610"
+    url "https://github.com/ycj3/agora-chat-cli/archive/v0.2.1.tar.gz"
+    sha256 "33fe02443d4805eac49bb19d62e6d8f7b213471654f87fd1cbd028ccd7645886"
     license "MIT"
   
     depends_on "go" => :build
