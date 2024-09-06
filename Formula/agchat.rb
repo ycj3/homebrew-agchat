2b9f42bfd90e92e2cc7081d8fa7e8cd31f85d4e7
Carlson <carlsonyuandev@gmail.com>
2024-09-06 14:17:40 +0000

    Update agchat to 0.3.3

--- Formula/agchat.rb
+++ Formula/agchat.rb
@@ -1,14 +1,14 @@
 class Agchat < Formula
     desc "A command-line interface for Agora Chat"
     homepage "https://github.com/ycj3/agora-chat-cli"
-    version "0.3.2"
+  version "0.3.3"
 
     if Hardware::CPU.arm?
-      url "https://github.com/ycj3/agora-chat-cli/releases/download/v0.3.2/agchat-0.3.2-macos-arm64.tar.gz"
-      sha256 "11ac8aea31110691d080feebdcc9fc7377cb953229eaf90363730b11154f7efa"
+    url "https://github.com/ycj3/agora-chat-cli/releases/download/v0.3.3/agchat-0.3.3-macos-arm64.tar.gz"
+    sha256 ""
     elsif Hardware::CPU.intel?
-      url "https://github.com/ycj3/agora-chat-cli/releases/download/v0.3.2/agchat-0.3.2-macos-amd64.tar.gz"
-      sha256 "11ac8aea31110691d080feebdcc9fc7377cb953229eaf90363730b11154f7efa"
+    url "https://github.com/ycj3/agora-chat-cli/releases/download/v0.3.3/agchat-0.3.3-macos-amd64.tar.gz"
+    sha256 ""
     end
 
     def install
