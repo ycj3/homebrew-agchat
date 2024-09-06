b7881f19727360b4575533276849acd33bbb7085
Carlson <carlsonyuandev@gmail.com>
2024-09-06 21:20:42 +0800

    Update homebrew formula to use a binary

--- Formula/agchat.rb
+++ Formula/agchat.rb
@@ -1,22 +1,20 @@
 class Agchat < Formula
     desc "A command-line interface for Agora Chat"
     homepage "https://github.com/ycj3/agora-chat-cli"
-    url "https://github.com/ycj3/agora-chat-cli/archive/v0.3.2.tar.gz"
-    sha256 "11ac8aea31110691d080feebdcc9fc7377cb953229eaf90363730b11154f7efa"
-    license "MIT"
-  
-    depends_on "go" => :build
-  
+    version "0.3.2"
+
+    if Hardware::CPU.arm?
+      url "https://github.com/ycj3/agora-chat-cli/releases/download/v0.3.2/agchat-0.3.2-macos-arm64.tar.gz"
+      sha256 "11ac8aea31110691d080feebdcc9fc7377cb953229eaf90363730b11154f7efa"
+    elsif Hardware::CPU.intel?
+      url "https://github.com/ycj3/agora-chat-cli/releases/download/v0.3.2/agchat-0.3.2-macos-amd64.tar.gz"
+      sha256 "11ac8aea31110691d080feebdcc9fc7377cb953229eaf90363730b11154f7efa"
+    end
+
     def install
-        if Hardware::CPU.arm?
-          system "go", "build", *std_go_args(ldflags: "-s -w -buildmode=pie")
-        elsif Hardware::CPU.intel?
-          system "go", "build", *std_go_args(ldflags: "-s -w")
-        end
-      end
-  
+      bin.install "agchat"
+    end
     test do
-      assert_match "agchat", shell_output("#{bin}/agchat --version")
+      assert_match version.to_s, shell_output("#{bin}/agchat --version")
     end
   end
-  
